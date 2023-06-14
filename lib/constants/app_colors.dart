03fc470c8b5362351fb56960635ece8a3f8ada0c
Mohammad <mohammadhprp@gmail.com>
2023-06-14 18:35:36 +0330

    feat: create an extension to get Color from hex color

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'extensions/color_hex_screen.dart';
+
+@immutable
+class AppColors {
+  static Color boardBackground = ColorHex.fromHex('#808080');
+
+  const AppColors._();
+}
